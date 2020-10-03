8d15bdee1850b21c9023064b2efd793b03a98795
Nikhil Adhau <nikhiladhau999@gmail.com>
2020-10-03 23:45:59 +0530

    Multithreaded Server

--- httpserver.py
+++ httpserver.py
@@ -1,3 +1,4 @@
+#! /usr/bin/python3
 '''
     HTTP server
     Author : Nikhil Adhau
@@ -8,14 +9,24 @@
 
 import sys
 from socket import *
+import threading
 
-PORT = 1300 
+def client_thread (clientSocket, address):
+    request_data = clientSocket.recv(1024).decode()
+    print (request_data)
+    string = "HTTP/1.1 200 OK\n\n<b> Hello There <!b>"
+    clientSocket.send(string.encode())
+    clientSocket.close()
+
+PORT = int (sys.argv[1]) 
 #create a INET(IPV4), (STREAM) TCP socket
 try : 
     serverSocket = socket (AF_INET, SOCK_STREAM)
 except error as msg:
     print (f"Socket can not be created: {msg}")
     sys.exit(1)
+#manipulating options for socket (REUSEADDR lets you use the port whose connection is shutting down)
+serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 #bind the socket to localhost and a port
 serverSocket.bind(('localhost', PORT))
 #start listening
@@ -25,5 +36,8 @@ print (f'HTTP server listning on {PORT}')
 while True:
     #accept outside connection
     (clientSocket, address) = serverSocket.accept()
+    thread = threading.Thread(target = client_thread, args = (clientSocket, address))
+    thread.start()
     print (f"New connection request from {address}")
-    clientSocket.close()
+
+serverSocket.close()
